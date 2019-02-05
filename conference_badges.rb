5054b32252195947951120ba8bcb1b616f405d73
Mariam Jaludi <mariam.jaludi@gmail.com>
2019-02-05 16:26:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,3 +9,12 @@ def batch_badge_creator(names)
   names.each do |name| batch_message.push("Hello, my name is #{name}"); end
   return batch_message
 end
+
+def assign_rooms(speakers)
+    room_assignments = []
+    speakers.each do |speaker| 
+      room_assignments.push("Hello, #{speaker}! You'll be assigned to room 
+      #{each_with_index + 1}!"); end
+      return room_assignments
+end
+      
