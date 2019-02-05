0ed252d60be76ba5149d3cbc8fbb3ac5ffccc18a
Mariam Jaludi <mariam.jaludi@gmail.com>
2019-02-05 16:33:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,7 +6,7 @@ end
 
 def batch_badge_creator(names)
   batch_message = []
-  names.each do |name| batch_message.push("Hello, my name is #{name}"); end
+  names.each do |name| batch_message.push("Hello, my name is #{name}."); end
   return batch_message
 end
 
